eab99fc998618b1ae8586d55d99f1ba2880c59ce
Ben Wilde <orangeducktape12@Gmail.com>
2017-04-11 17:14:39 -0400

    Progress

--- Monopoly.py
+++ Monopoly.py
@@ -10,17 +10,31 @@ def run(*args):
 
 root = Tk()
 
+def onePlayer():
+    displayBox.insert(END,"You have selected one player mode")
+
+
+def twoPlayer():
+    displayBox.insert(END,"You have selected two player mode")
+
 displayBox = Text(root)
 displayBox.pack(padx = 10, pady = 10)
 displayBox.insert(END,">>> Monopoly Game \n\n")
 
-def blink():
-    root.after(1000, displayBox.insert(END,".\n\n"))
-
-blink()
-
-
 displayBox.insert(END,"Welcome to Monopoly. Do you want to play 1 or 2 player?\n\n")
+
+eBox = Entry(root)
+eboxval = eBox.get()
+eBox.pack()
+
+if eboxval == "1":
+    onePlayer()
+
+elif eboxval == "2":
+    twoPlayer()
+
+else:
+    displayBox.insert(END,"***Input Invalid***")
 
 
 
